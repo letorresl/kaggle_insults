39c1d172f8100c04ee7ee0589af88dc6047e9f7e
Luis Torres <luis.e.torreslopez@gmail.com>
2014-07-01 21:21:01 -0500

    Modificacion de archivo train.py incluyendo la actualizacion de scikit learn como esta visto en su correspondiente archivo de ipynb

--- train.py
+++ train.py
@@ -1,7 +1,12 @@
+# coding: utf-8
 import numpy as np
 import time
+from socket import gethostname
+from pytz import timezone
+from datetime import datetime
 from sklearn.metrics import roc_auc_score, make_scorer
 from multiprocessing import cpu_count
+from time import strftime, gmtime
 from sklearn.feature_extraction.text import CountVectorizer
 #from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.cross_validation import train_test_split
@@ -65,22 +70,22 @@ def write_test(labels, fname="test_prediction.csv"):
 
 def grid_search():
     tracer = Tracer()
+    
     comments, dates, labels = load_data()
-
     # get the google bad word list
     with open("google_badlist.txt") as f:
         badwords = [l.strip() for l in f.readlines()]
         badword_doc = " ".join(badwords)
-
+    
     comments.append(badword_doc)
-
+    
     #countvect = CountVectorizer(max_n=2, analyzer="char")
     countvect = CountVectorizer(ngram_range=(1,2))
     counts = countvect.fit_transform(comments).tocsr()
     badword_counts = counts[-1, :]
     counts = counts[:-1, :]
     comments.pop(-1)
-
+    
     ## some handcrafted features!
     n_words = [len(c.split()) for c in comments]
     n_chars = [len(c) for c in comments]
@@ -93,23 +98,23 @@ def grid_search():
     mean_word_len = [np.mean([len(w) for w in c.split()]) for c in comments]
     # number of google badwords:
     n_bad = counts * badword_counts.T
-
+    
     features = np.array([n_words, n_chars, allcaps, max_word_len,
         mean_word_len, n_bad.toarray()]).T
-
+    
     features = sparse.hstack([counts, features])
-
+    
     ## exlamation marks
     #excl = [comment.count("!") for comment in comments]
     ## double exlamation marks
     #excl2 = [comment.count("!!") for comment in comments]
-
+    
     #countvect = TfidfVectorizer()
-
+    
     print("vectorizing")
     #counts_array = counts.toarray()
     #indicators = sparse.csr_matrix((counts_array > 0).astype(np.int))
-
+    
     #X_train, X_test, y_train, y_test = train_test_split(counts, labels,
                                                         #test_size=0.5)
     #inds = np.random.permutation(len(labels))
@@ -121,31 +126,75 @@ def grid_search():
     #clf = LinearSVC(tol=1e-8, penalty='l1', dual=False, C=0.5)
     clf = LogisticRegression(tol=1e-8)
     #pipeline = Pipeline([('vect', countvect), ('logr', clf)])
-
+    
     #clf = NearestCentroid()
-
+    
     #param_grid = dict(max_depth=np.arange(1, 10))
     #clf = RandomForestClassifier(n_estimators=10)
-
+    
     puntuador = make_scorer(roc_auc_score, greater_is_better = True)
     numerocpus = cpu_count()
-    grid = GridSearchCV(clf, cv=2, param_grid=param_grid, verbose=4, scoring=puntuador,
+    if gethostname() == 'Profeta':
+        crossvalidaciones = 2
+    elif numerocpus >= 4:
+        crossvalidaciones = 5
+    else:
+        crossvalidaciones = 2
+    grid = GridSearchCV(clf, cv=crossvalidaciones, param_grid=param_grid, verbose=4, scoring=puntuador,
             n_jobs=numerocpus)
     #print(cross_val_score(clf, counts, labels, cv=3))
-
+    
     #grid.fit(counts, labels)
     grid.fit(features, labels)
     #clf.fit(X_train, y_train)
     #print(clf.score(X_test, y_test))
     #tracer()
+    
     comments_test, dates_test = load_test()
-    counts_test = countvect.transform(comments_test)
-    prob_pred = grid.best_estimator_.predict_proba(counts_test)
+    comments_test.append(badword_doc)
+    counts_test = countvect.transform(comments_test).tocsr()
+    
+    badword_counts = counts_test[-1, :]
+    counts_test = counts_test[:-1, :]
+    comments_test.pop(-1)
+    
+    ## some handcrafted features!
+    n_words = [len(c.split()) for c in comments_test]
+    n_chars = [len(c) for c in comments_test]
+    # number of uppercase words
+    allcaps = [np.sum([w.isupper() for w in comment.split()])
+           for comment in comments_test]
+    # longest word
+    max_word_len = [np.max([len(w) for w in c.split()]) for c in comments_test]
+    # average word length
+    mean_word_len = [np.mean([len(w) for w in c.split()]) for c in comments_test]
+    # number of google badwords:
+    n_bad = counts_test * badword_counts.T
+    
+    features_test = np.array([n_words, n_chars, allcaps, max_word_len,
+        mean_word_len, n_bad.toarray()]).T
+    
+    features_test = sparse.hstack([counts_test, features_test])
+    
+    
+    prob_pred = grid.best_estimator_.predict_proba(features_test)
+    print(prob_pred)
     write_test(prob_pred[:, 1])
+    return grid
+    
 
 if __name__ == "__main__":
     inicio = time.time()
-    grid_search()
+    rejilla = grid_search()
     final = time.time()
     tiempoTotal = final - inicio
+    
+    tiempoMexico = timezone('America/Mexico_City')
+    
+    
     print("El tiempo de ejecucion transcurrido fue de: " + str(tiempoTotal) + " segundos.")
+    with open("resultados.txt",'a') as archivo:
+        archivo.write("*** " + str(datetime.now(tiempoMexico)) + "\n\n")
+        archivo.write("\tEl tiempo de ejecucion transcurrido fue de: " + str(tiempoTotal) + " segundos.\n")
+        archivo.write ("\tEl modelo utilizado fue:\n\t\t" + str(rejilla.best_estimator_) )
+        archivo.write("\n\tEl cual tuvo una AUC de: " + str(rejilla.best_score_) + "\n\n")
